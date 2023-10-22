8d768244777526314027f1da2b630c8bf306b5f9
philiprockert <philiprockert@gmail.com>
2023-10-22 10:55:49 -0500

    configurando

--- server.js
+++ server.js
@@ -3,27 +3,42 @@ const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = 8080
-const connection = mysql.createConnection({
-  host: 'containers-us-west-183.railway.app',
+
+
+function obtenerDatosDesdeBD(callback) {
+  const db = mysql.createConnection({
+    host: 'containers-us-west-183.railway.app',
   port: '5439',
   user: 'root',
   password: 'l8buMHicvpHvn5EIhxne',
   database: 'railway'
-});
+  });
+
+  db.connect((err) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      const consultaSQL = 'SELECT * FROM usuario';
+      db.query(consultaSQL, (err, resultados) => {
+        db.end(); // Cierra la conexión después de la consulta
+        callback(err, resultados);
+      });
+    }
+  });
+}
+
+// Luego, puedes utilizar la función obtenerDatosDesdeBD en tu ruta de Express
+
+
+
 app.use(bodyParser.json());
 
 
 
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err);
-  } else {
-    console.log('Conexión exitosa a la base de datos MySQL');
-  }
-});
 
-module.exports = connection;
+
+
 app.post('/crear-datos', (req, res) => {
   const { nombre, email, password } = req.body; // Los campos deben coincidir con la estructura de tu tabla
   const consultaSQL = 'INSERT INTO usuario ( nombre, email, password) VALUES (?, ?, ?)';
@@ -41,15 +56,14 @@ app.post('/crear-datos', (req, res) => {
 
 
 app.get('/', (req, res) => {
-  const consultaSQL = 'SELECT * FROM usuario';
-  connection.query(consultaSQL, (err, resultados) => {
+  obtenerDatosDesdeBD((err, resultados) => {
     if (err) {
       console.error('Error al consultar la base de datos: ' + err);
       res.status(500).json({ error: 'Error interno del servidor' });
     } else {
       res.json(resultados);
     }
-  });
+  })
 });
 
 app.listen(PORT)
