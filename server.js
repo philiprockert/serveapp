2a4e4164c432f724f86626577157cc097c3b1d67
philiprockert <philiprockert@gmail.com>
2023-10-22 09:54:50 -0500

    conexion con base de datos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express')
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+
 const connection = mysql.createConnection({
   host: 'containers-us-west-183.railway.app',
   port: '5439',
@@ -54,4 +54,3 @@ app.get('/obtener-datos', (req, res) => {
 
 
 
-app.listen(port)
