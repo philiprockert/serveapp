7f6f04be9b61accf890478eaa541bc0b5ceecf07
philiprockert <philiprockert@gmail.com>
2023-10-29 17:24:58 -0500

    crear datos

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 require('dotenv').config();
 const PORT = 8080
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 
 
 app.use(bodyParser.json());
