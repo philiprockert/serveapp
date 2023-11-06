4a0425c39d1e8994e598aca3e81a4e7218447c49
philiprockert <philiprockert@gmail.com>
2023-11-06 10:43:33 -0500

    creacion exitosa

--- server.js
+++ server.js
@@ -1,75 +1,54 @@
-const express= require('express')
-const mysql = require('mysql2')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 require('dotenv').config();
+
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Si necesitas enviar cookies o credenciales
+  credentials: true,
 }));
 
-
-const PORT = 8080
-
-const pool = mysql.createPool(dbConfig);
-
-const promisePool = pool.promise();
-
+app.options('/crear-datos', (req, res) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(200).send();
+});
 
 app.use(bodyParser.json());
 
-function obtenerDatosDesdeBD(callback) {
-  const db = mysql.createConnection({
+const dbConfig = {
   host: process.env.DBHOST,
   port: process.env.DBPORT,
   user: process.env.DBUSER,
   password: process.env.DBPASS,
   database: process.env.DBNAME
-  });
+};
 
-  db.connect((err) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      const consultaSQL = 'SELECT * FROM usuario';
-      db.query(consultaSQL, (err, resultados) => {
-        db.end(); // Cierra la conexión después de la consulta
-        callback(err, resultados);
-      });
-    }
-  });
-}
+const pool = mysql.createPool(dbConfig);
+const promisePool = pool.promise();
+
 // Función para insertar datos en la base de datos
 function insertarDatosEnBD(datos, callback) {
-  const db = mysql.promisePool({ host: process.env.DBHOST,
-    port: process.env.DBPORT,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    database: process.env.DBNAME
-   
-  });
+  const { nombre, email, password } = datos;
+  const consultaSQL = 'INSERT INTO usuario (nombre, email, password) VALUES (?, ?, ?)';
 
-  db.connect((err) => {
-    if (err) {
+  promisePool.query(consultaSQL, [nombre, email, password])
+    .then(([resultado]) => {
+      callback(null, resultado);
+    })
+    .catch((err) => {
       callback(err, null);
-    } else {
-      const {nombre, email, password } = datos;
-      const consultaSQL = 'INSERT INTO usuario (nombre, email, password) VALUES (?, ?, ?)';
-      
-      db.query(consultaSQL, [nombre, email, password], (err, resultado) => {
-        db.end(); // Cierra la conexión después de la consulta
-        callback(err, resultado);
-      });
-    }
-  });
+    });
 }
-// Luego, puedes utilizar la función obtenerDatosDesdeBD en tu ruta de Express
-app.post('/crear-datos', (req, res) => {
-  const {id,  nombre, email, password } = req.body;
 
-  insertarDatosEnBD({id,  nombre, email, password }, (err, resultado) => {
+// Ruta para insertar datos
+app.post('/crear-datos', (req, res) => {
+  const { nombre, email, password } = req.body;
+
+  insertarDatosEnBD({ nombre, email, password }, (err, resultado) => {
     if (err) {
       console.error('Error al insertar datos en la base de datos: ' + err);
       res.status(500).json({ error: 'Error interno del servidor' + err });
@@ -79,16 +58,21 @@ app.post('/crear-datos', (req, res) => {
   });
 });
 
+// Ruta para obtener datos
 app.get('/', (req, res) => {
-  obtenerDatosDesdeBD((err, resultados) => {
-    if (err) {
+  const consultaSQL = 'SELECT * FROM usuario';
+
+  promisePool.query(consultaSQL)
+    .then(([resultados]) => {
+      res.json(resultados);
+    })
+    .catch((err) => {
       console.error('Error al consultar la base de datos: ' + err);
       res.status(500).json({ error: 'Error interno del servidor' });
-    } else {
-      res.json(resultados);
-    }
-  })
+    });
 });
 
-app.listen(PORT)
-
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor Express en ejecución en el puerto ${PORT}`);
+});
